b426e570860758e3292868ecde1c34034073d147
Greg L. Turnquist <gturnquist@vmware.com>
2022-10-20 19:04:43 -0500

    Polish

--- src/main/java/com/example/springdatacassandraobservability/SpringDataCassandraObservabilityApplication.java
+++ src/main/java/com/example/springdatacassandraobservability/SpringDataCassandraObservabilityApplication.java
@@ -1,7 +1,7 @@
 package com.example.springdatacassandraobservability;
 
+import io.micrometer.observation.Observation;
 import io.micrometer.observation.ObservationRegistry;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,10 +18,18 @@ public class SpringDataCassandraObservabilityApplication {
 
 	// Pre-load data
 	@Bean
-	CommandLineRunner initData(EmployeeRepository repository, ObservationRegistry registry) {
-		return args -> {
-			repository.save(new Employee("1", "Frodo", "ring bearer"));
-			repository.save(new Employee("2", "Bilbo", "burglar"));
-		};
+	CommandLineRunner initData(EmployeeRepository repository, ObservationRegistry registry) { // <1>
+		Observation observation = Observation.start("init-database", registry); // <2>
+		try (Observation.Scope scope = observation.openScope()) { // <3>
+			return args -> {
+				repository.save(new Employee("1", "Frodo", "ring bearer"));
+				repository.save(new Employee("2", "Bilbo", "burglar"));
+			};
+		} catch (Exception e) {
+			observation.error(e); // <4>
+			throw e;
+		} finally {
+			observation.stop(); // <5>
+		}
 	}
 }
